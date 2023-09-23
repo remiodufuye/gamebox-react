eabda9df9de28f081ca5484576371be9ea00f08a
remiodufuye <remi.odufuye@gmail.com>
2023-09-23 15:37:49 -0400

    added button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: string;
+  color?: "primary" | "secondary" | "danger";
+  onClick: () => void;
+}
+
+const Button = ({ children, onClick, color = "danger" }: Props) => {
+  return (
+    <button type="button" className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
