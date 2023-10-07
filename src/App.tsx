63445d9d13e9fc0ccc32b9450b08ec422cfc0286
remiodufuye <remi.odufuye@gmail.com>
2023-10-07 15:08:24 -0400

    updating state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
-import NavBar from "./components/navBar";
-import Cart from "./components/Cart";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
-  return (
-    <div>
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
-    </div>
-  );
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
+  });
+
+  const handleClick = () => {
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
+  };
 }
 
 export default App;
