6a3a8f5af78fd615b8c230883dd85480e4b052c5
remiodufuye <remi.odufuye@gmail.com>
2023-10-07 19:46:45 -0400

    build expandable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import { useState } from "react";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product 1", quantity: 1 },
-      { id: 2, title: "Product 2", quantity: 1 },
-    ],
-  });
-
-  const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
-      ),
-    });
-  };
+  return (
+    <div>
+      <ExpandableText>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis itaque
+        fugiat tempore, expedita mollitia quos eligendi cumque, ab est numquam
+        officia veniam veritatis maxime debitis asperiores sed eveniet atque
+        voluptas libero ratione iste ea. Laborum, nulla. Minima nam magni totam
+        quas id sit fugit labore laboriosam accusamus et eos necessitatibus
+        molestias velit dolores ad itaque ratione ullam, in incidunt facilis a
+        autem aspernatur perferendis. Enim ratione dolore alias est dicta aut
+        consequuntur. Voluptates incidunt, accusantium ex tempore, est dicta
+        quam molestias modi vitae quo voluptatem, earum minus. Eligendi
+        asperiores, perferendis nulla ipsum consectetur non nemo veritatis
+        omnis, consequuntur obcaecati esse.
+      </ExpandableText>
+    </div>
+  );
 }
 
 export default App;
