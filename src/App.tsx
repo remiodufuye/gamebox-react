04427d06dc00f06a88e31a68846047ba4f3e3041
remiodufuye <remi.odufuye@gmail.com>
2023-10-16 20:41:33 -0400

    Created Submission Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import { useState } from "react";
 import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis itaque
-        fugiat tempore, expedita mollitia quos eligendi cumque, ab est numquam
-        officia veniam veritatis maxime debitis asperiores sed eveniet atque
-        voluptas libero ratione iste ea. Laborum, nulla. Minima nam magni totam
-        quas id sit fugit labore laboriosam accusamus et eos necessitatibus
-        molestias velit dolores ad itaque ratione ullam, in incidunt facilis a
-        autem aspernatur perferendis. Enim ratione dolore alias est dicta aut
-        consequuntur. Voluptates incidunt, accusantium ex tempore, est dicta
-        quam molestias modi vitae quo voluptatem, earum minus. Eligendi
-        asperiores, perferendis nulla ipsum consectetur non nemo veritatis
-        omnis, consequuntur obcaecati esse.
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
