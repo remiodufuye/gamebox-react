00896e11cbf60859e58cc49d86f50c4039646dc2
github username <github email address>
2023-12-14 21:25:03 -0500

    Effect CleanUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import {useEffect, useState } from "react";
-import ProductList from "./components/productList";
+import {useEffect } from "react";
 
 function App() {
-  const [category , setCategory] = useState('')
+  useEffect(() => {
+    document.title = "My App" ;
+  })
 
   return (
     <div>
-      <select className="form-select" onChange={(event)=>setCategory(event.target.value)}>
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category}/>
     </div>
   )
 }
