fe765912355e325a23dc38a888affbd81ddc8653
github username <github email address>
2023-12-15 19:43:15 -0500

    changes made

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import {useEffect } from "react";
 
+const connect = () => console.log('Connecting...');
+const disconnect = () => console.log('Disconnecting...');
+
 function App() {
   useEffect(() => {
-    document.title = "My App" ;
+    connect()
+    return () => disconnect() ; 
   })
 
   return (
