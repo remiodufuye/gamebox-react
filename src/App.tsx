76212dc01db5950db2fc172e2ea9524d19b556a7
remiodufuye <remi.odufuye@gmail.com>
2023-12-27 10:49:52 -0500

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,32 @@
-import axios from 'axios';
-import {useState,useEffect} from 'react';
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 interface User {
   id: number;
-  name:string ;
-
+  name: string;
 }
 
 function App() {
- const [users,setUsers] = useState<User[]>([]) ;
-const [error,setError] = useState('')
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
- useEffect(() => {
-  axios.get<User[]>('https://jsonplaceholder.typicode.com/xusers')
-  .then(res => setUsers(res.data))
-  .catch(err => setError(err.message));
- },[])
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.message));
+  }, []);
 
-  return 
+  return;
   <>
-  {error && <p className="text-danger">{error}</p>}
-  <ul>
-    {users.map(user =><li key={user.id}>{user.name}</li>)}
-
-  </ul> ;
-  </>
+    {error && <p className="text-danger">{error}</p>}
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>{" "}
+    ;
+  </>;
 }
 
 export default App;
