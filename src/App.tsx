360e4d56517dda84948b5b98e2295b954df05eea
remiodufuye <remi.odufuye@gmail.com>
2023-12-28 15:50:18 -0500

    Canceled Fetch Request

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios, { CanceledError } from "axios";
 import { useState, useEffect } from "react";
 
 interface User {
@@ -11,21 +11,19 @@ function App() {
   const [error, setError] = useState("");
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const res = await axios.get<User[]>(
-          "https://jsonplaceholder.typicode.com/xusers"
-        );
-        setUsers(res.data);
-      } catch (err) {
-        setError((err as AxiosError).message);
-      }
-    };
+    const controller = new AbortController();
 
-    fetchUsers();
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => setUsers(res.data))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
 
-    // .then((res) => setUsers(res.data))
-    // .catch((err) => setError(err.message));
+    return () => controller.abort();
   }, []);
 
   return;
